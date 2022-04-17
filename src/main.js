969e21c038e6362e1a80576ae8034ca29b3bbe2c
adventure-boy <zjy521995@163.com>
2022-04-17 23:10:17 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router/index.js'
-import axios from 'axios'
-axios.defaults.baseURL="http://localhost:8080"
+
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
