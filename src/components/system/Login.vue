958a9cdd286e60a6b6435b2850ef5804a26de694
adventure-boy <zjy521995@163.com>
2022-04-19 22:55:02 +0800

    登录页

--- src/components/system/Login.vue
+++ src/components/system/Login.vue
@@ -1,5 +1,20 @@
 <template>
-
+  <el-form  label-width="100px">
+    <el-form-item label="账号">
+      <el-input type="password" v-model="password" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input type="password" v-model="password" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="验证码" >
+      <el-input v-model="verificationCode" ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+      <!-- <el-button @click="resetForm('ruleForm')">重置</el-button> -->
+    </el-form-item>
+  </el-form>
+</template>
 
 <script>
 import axios from 'axios'
@@ -29,5 +44,13 @@ export default {
 </script>
 
 <style>
-  
+  .el-form{
+    MARGIN: 200PX;
+    text-align: center;
+  }
+  .el-form-item{
+    text-align: center;
+    width: 500px;
+    margin-left: 20%;
+  }
 </style>
